8b9f3dd2431b68e8aa4db7acd3deec11fefc3591
Zh-Oleksandr <160198019+Zh-Oleksandr@users.noreply.github.com>
2024-06-12 15:41:01 -0700

    Initial commit

---
+++ Assets/Scripts/UniversalMovement.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.U2D;
+using UnityEngine;
+
+public class UniversalMovement : MonoBehaviour
+{
+    public GameObject Controller;
+
+    public bool car = true;
+    public bool tank = false;
+    public bool other = false;
+
+    public Rigidbody2D rb;
+    public float maxspeed = 10f;
+    public float speed = 0f;
+    public float accel = 3f;
+
+    public float deccel = 3f;
+
+    private Vector2 movedirection;
+    private float angle = Mathf.PI * 1/2;
+    public void CarMoveForeward()
+    {
+        if (speed < 0)
+        {
+            speed += (accel + deccel) * Time.deltaTime;
+        }
+        else if (speed < maxspeed)
+        {
+            speed += accel * Time.deltaTime;
+        }
+    }
+    public void CarMoveBackward()
+    {
+        if (speed > 0)
+        {
+            speed -= (accel + deccel) * Time.deltaTime;
+        }
+        else if (Mathf.Abs(speed) < maxspeed)
+        {
+            speed -= accel * Time.deltaTime;
+        }
+    }
+
+    public void CarMoveRight()
+    {
+        if (speed > 0)
+        {
+            angle -= Mathf.PI * 1 / 4 * Time.deltaTime;
+        }
+        else if (speed < 0)
+        {
+            angle += Mathf.PI * 1 / 3 * Time.deltaTime;
+        }
+    }
+    public void CarMoveLeft()
+    {
+        if (speed > 0)
+        {
+            angle += Mathf.PI * 1 / 4 * Time.deltaTime;
+        }
+        else if (speed < 0)
+        {
+            angle -= Mathf.PI * 1 / 3 * Time.deltaTime;
+        }
+    }
+
+    public void CarSlowDown()
+    {
+
+        if (speed > 0)
+        {
+            speed -= deccel * Time.deltaTime;
+        }
+        else if (speed < 0)
+        {
+            speed += deccel * Time.deltaTime;
+        }
+        if(Mathf.Abs(speed - 0) <= 0.005)
+        {
+            speed = 0;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotationvector = new Vector3(0, 0, angle * 180/Mathf.PI);
+        Quaternion rotation = Quaternion.Euler(rotationvector);
+        rb.transform.rotation = rotation;
+        movedirection = new Vector3(1 * Mathf.Cos(angle), 1 * Mathf.Sin(angle));
+        if (car)
+        {
+            tank = false;
+            other = false;
+            rb.velocity = movedirection * speed;
+
+        }
+        else if (tank)
+        {
+            car = false;
+            other = false;
+            rb.velocity = movedirection * speed;
+        }
+        else if (other)
+        {
+            car = false;
+            tank = false;
+        }
+        
+    }
+}
