6b977b2176ea7cf10b81fe5f26ba263449076772
Zh-Oleksandr <160198019+Zh-Oleksandr@users.noreply.github.com>
2024-06-14 15:54:56 -0700

    added swapping

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -1,19 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
     public UniversalMovement movement;
-    // Start is called before the first frame update
+    public GameObject newhull = null;
+
+    public float interactionradius = 1;
+
     void Start()
     {
+        interactionradius = this.GetComponent<CircleCollider2D>().radius;
         movement = this.GetComponentInParent<UniversalMovement>();
+        movement.Controller = this.gameObject;
     }
 
-    // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "Chasse")
+        {
+            newhull = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (newhull != null)
+        {
+            if (Vector3.Distance(this.transform.position, newhull.transform.position) > interactionradius)
+            {
+                newhull = null;
+            }
+        }     
+    }
+
     void Update()
     {
+        if (newhull != null)
+        {
+            if (Input.GetKey(KeyCode.F))
+            {
+                movement.Controller = null;
+                movement = newhull.GetComponent<UniversalMovement>();
+                movement.dead = false;
+                this.gameObject.transform.SetParent(newhull.transform);
+                movement.Controller = this.gameObject;
+                newhull = null;
+            }
+        }
         if (movement.car == true)
         {
             if (Input.GetKey(KeyCode.W))
