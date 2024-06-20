b0afe05527c53ec5d619fbfb92747b3c1e2de4ce
Zh-Oleksandr <160198019+Zh-Oleksandr@users.noreply.github.com>
2024-06-19 22:45:03 -0700

    aa

--- Assets/Scripts/SlotScript.cs
+++ Assets/Scripts/SlotScript.cs
@@ -7,23 +7,18 @@ public class SlotScript : MonoBehaviour
 {
     public GameObject weapon;
     public int slotsize = 2;
-    public Rigidbody2D rb;
 
     private float angle;
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody2D>();
+        ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition; ;
-        angle = (mousePos.y - rb.transform.position.y) / (mousePos.x - rb.transform.position.y) * 180/Mathf.PI;
-        Vector3 direction = new Vector3(0, 0, angle);
-
-        rb.transform.rotation = Quaternion.Euler(direction);
+        
        
     }
 }
