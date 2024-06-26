3fa8c9b5b79518137e61a1b8af6f200209552356
Zh-Oleksandr <160198019+Zh-Oleksandr@users.noreply.github.com>
2024-06-25 18:22:51 -0700

    aaa

--- Assets/Scripts/ChasseScript.cs
+++ Assets/Scripts/ChasseScript.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ChasseScript : MonoBehaviour
 {
     public GameObject[] Slots;
     public Vector3[] slotpos;
 
+    public GameObject thisgameobject;
+    public UniversalMovement universalMovement;
+    public GameObject inventory;
+
     public GameObject frontarmor;
     public GameObject backarmor;
     public GameObject leftarmor;
     public GameObject rightarmor;
 
+    public GameObject inventorycheck;
+    private bool inventoryopen = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        thisgameobject = this.gameObject;
+        universalMovement = this.GetComponent<UniversalMovement>();
         for (int i = 0; i < Slots.Length; i++)
         {
             Slots[i].transform.position = this.GetComponentInParent<Transform>().position + slotpos[i];
@@ -25,6 +35,21 @@ public class ChasseScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I) && universalMovement.notai == true)
+        {
+            if (!inventoryopen)
+            {
+                inventoryopen = true;
+                inventorycheck = Instantiate(inventory, this.transform.position - new Vector3(1f, 0f, 0f), Quaternion.Euler(0f, 0f, 0f));
+            }
+            else
+            {
+                if(inventorycheck != null)
+                {
+                    Destroy(inventorycheck);
+                    inventoryopen = false;
+                }
+            }
+        }
     }
 }
